07352f89d78ad8acd236cb59210353be7108f9c1
Matias <matiasm0027@gmail.com>
2021-03-25 17:35:55 +0100

    ProjectoU1(crear registro nuevo)

--- ProjectoU1/src/projectou1/ProjectoU1.java
+++ ProjectoU1/src/projectou1/ProjectoU1.java
@@ -6,6 +6,7 @@
 package projectou1;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 /**
@@ -55,4 +56,42 @@ public class ProjectoU1 {
 
         }
     }
+    private static void crearRegistrov2(File fichero) {
+        Scanner lector = new Scanner(System.in);
+        String registroNuevo;
+        String[] cambiar = new String[3];
+        System.out.println("Crear un nuevo registro");
+        System.out.print("ID: ");
+        String id = lector.next().trim();
+        System.out.print("Nombre de aula: ");
+        String nombreAula = lector.next().trim();
+        lector.nextLine();
+        System.out.print("Capacidad de alumnos: ");
+        String cantidadAlumnos = lector.next().trim();
+        System.out.print("Hay pc's: ");
+        cambiar[0] = lector.next().trim();
+        System.out.print("Cantidad de pc's disponibles: ");
+        String cantidadPc = lector.next().trim();
+        System.out.print("Hay proyector: ");
+        cambiar[1] = lector.next().trim();
+        System.out.print("El aula esta insonorizada: ");
+        cambiar[2] = lector.next().trim();
+
+        for (int i = 0; i < cambiar.length; i++) {
+            cambiar[i] = cambiar[i].substring(0, 1).toUpperCase() + cambiar[i].charAt(1);
+        }
+        id = id.substring(0, 1).toUpperCase() + id.substring(1);
+        nombreAula = nombreAula.substring(0, 1).toUpperCase() + nombreAula.substring(1);
+        registroNuevo =id + "," + nombreAula + "," + cantidadAlumnos + "," + cambiar[0] + "," + cantidadPc + "," + cambiar[1] + "," + cambiar[2];
+        try {
+            FileWriter writer = new FileWriter(fichero, true);
+
+            writer.write(registroNuevo);
+
+            writer.close();
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error al crear/escribir en el fichero");
+        }
+    }
 }
+
