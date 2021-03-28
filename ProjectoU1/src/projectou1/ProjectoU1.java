61352a24ff2c833099642321d50f93fef61807e6
Matias <matiasm0027@gmail.com>
2021-03-28 17:05:57 +0200

    ProjectoU1(comentado y editado)

--- ProjectoU1/src/projectou1/ProjectoU1.java
+++ ProjectoU1/src/projectou1/ProjectoU1.java
@@ -25,17 +25,16 @@ public class ProjectoU1 {
     }
 
     /**
-     * Esta funcion muestra los datos de las aulas
-     *
-     * @param fichero archivo classroom.txt donde se encuentan los datos
+     * Funcion que permite leer el fichero
+     * @param fichero archivo classroom.txt donde se alnacenan los datos de las
+     * aulas
      */
     private static void LeerClassroom(File fichero) {
         int numLineas = 0;
-
         try {
             // Codificación ISO-8859-1 (ANSI) o UTF-8 dependiendo de cómo esté creado el fichero de texto
             Scanner lectorFichero = new Scanner(fichero, "UTF-8");
-
+            //bucle que lee linea a linea el fichero           
             while (lectorFichero.hasNext()) {
                 numLineas++;
                 String[] linea = lectorFichero.nextLine().split(",");
@@ -48,7 +47,6 @@ public class ProjectoU1 {
                 System.out.println("Insonorizada: " + linea[6]);
                 System.out.println("");
             }
-
             lectorFichero.close();
             System.out.println("Hay " + numLineas + " registros de aulas");
         } catch (Exception e) {
@@ -56,7 +54,14 @@ public class ProjectoU1 {
 
         }
     }
-    private static void crearRegistrov2(File fichero) {
+
+    /**
+     * Esta funcion se encarga de crear un nuevo registro de un aula
+     *
+     * @param fichero archivo classroom.txt donde se almacenan los datos de las
+     * aulas
+     */
+    private static void crearRegistro(File fichero) {
         Scanner lector = new Scanner(System.in);
         String registroNuevo;
         String[] cambiar = new String[3];
@@ -68,30 +73,32 @@ public class ProjectoU1 {
         lector.nextLine();
         System.out.print("Capacidad de alumnos: ");
         String cantidadAlumnos = lector.next().trim();
-        System.out.print("Hay pc's: ");
+        System.out.print("Hay pc's(Si/No): ");
         cambiar[0] = lector.next().trim();
         System.out.print("Cantidad de pc's disponibles: ");
         String cantidadPc = lector.next().trim();
-        System.out.print("Hay proyector: ");
+        System.out.print("Hay proyector(Si/No): ");
         cambiar[1] = lector.next().trim();
-        System.out.print("El aula esta insonorizada: ");
+        System.out.print("El aula esta insonorizada(Si/No): ");
         cambiar[2] = lector.next().trim();
 
+        //Poner las primeras letras en mayuscula
         for (int i = 0; i < cambiar.length; i++) {
             cambiar[i] = cambiar[i].substring(0, 1).toUpperCase() + cambiar[i].charAt(1);
         }
         id = id.substring(0, 1).toUpperCase() + id.substring(1);
         nombreAula = nombreAula.substring(0, 1).toUpperCase() + nombreAula.substring(1);
-        registroNuevo =id + "," + nombreAula + "," + cantidadAlumnos + "," + cambiar[0] + "," + cantidadPc + "," + cambiar[1] + "," + cambiar[2];
+        //String donde se guarda la linea de datos en formato CSV
+        registroNuevo = "\n" + id + "," + nombreAula + "," + cantidadAlumnos + "," + cambiar[0] + "," + cantidadPc + "," + cambiar[1] + "," + cambiar[2];
+
         try {
+            //agregar una nueva linea al fichero(true para agregar una linea y no sobreescribirla)
             FileWriter writer = new FileWriter(fichero, true);
-
             writer.write(registroNuevo);
-
             writer.close();
+            System.out.println("El registro ha sido creado con exito");
         } catch (Exception e) {
             System.out.println("Ha ocurrido un error al crear/escribir en el fichero");
         }
     }
 }
-
